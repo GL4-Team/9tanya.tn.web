4334f824451f00bf8849d01918c6c85d723ca5ef
lassmed <mohamed.lasswed2001@gmail.com>
2024-01-16 20:33:04 +0100

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import {FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+import {HttpClientModule} from "@angular/common/http";
+import {MovieApiServiceService} from "./service/movie-api-service.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [MovieApiServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/movie-details/movie-details.component.ts
+++ src/app/pages/movie-details/movie-details.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MovieApiServiceService} from "../../service/movie-api-service.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.css']
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit{
+  constructor(private service:MovieApiServiceService, private activatedRoute:ActivatedRoute) {
+  }
+  getMovieDetailResult:any;
+  getMovieVideoResult:any
+  getMovieCastResult:any
+  ngOnInit() {
+    let getParamId=this.activatedRoute.snapshot.paramMap.get('id');
+    this.getMovie(getParamId);
+    this.getVideo(getParamId)
+    this.getMovieCast(getParamId)
+  }
+
+  getMovie(id:any){
+    this.service.getMovieDetails(id).subscribe((result)=>{
+      console.log(result,'getmoviedetails#')
+      this.getMovieDetailResult=result;
+    })
+  }
+  getVideo(id:any)
+  {
+    this.service.getMovieVideo(id).subscribe((result)=>{
+      console.log(result,'getmovieVideo#')
+      result.results.forEach((element:any)=>{
+        if(element.type=="Trailer"){
+          this.getMovieVideoResult=element.key;
+        }
+      })
+    })
+  }
+
+  getMovieCast(id:any){
+    this.service.getMovieCast(id).subscribe((result)=>{
+      console.log(result,'movieCast#');
+      this.getMovieCastResult=result.cast;
+    })
+  }
 
 }
