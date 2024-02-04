fb8bc44ccd54d04108c9d82ab5ceee9e1cf76573
lassmed <mohamed.lasswed2001@gmail.com>
2024-02-04 21:03:29 +0100

    search optimisation

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {MovieApiService} from "../../service/movie-api.service";
+import {debounceTime, distinctUntilChanged, filter} from "rxjs";
 
 @Component({
   selector: 'app-search',
@@ -8,20 +9,31 @@ import {MovieApiService} from "../../service/movie-api.service";
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit{
+  searchResult:any;
+  searchForm=new FormGroup({
+    'movieName':new FormControl()
+  });
   constructor(private service :MovieApiService) {
   }
   ngOnInit() {
+    this.searchForm.get('movieName')?.valueChanges.pipe(
+      debounceTime(400), // Wait for 400ms pause in events
+      filter(text => text.length > 2), // Only proceed if the text length is greater than 2
+      distinctUntilChanged() // Only proceed if the new value is different from the last
+    ).subscribe(value => {
+      this.performSearch(value);
+    });
   }
-  searchResult:any;
-  searchForm=new FormGroup({
-    'movieName':new FormControl(null)
-  });
-  submitForm(){
-    console.log(this.searchForm.value,'searchform#');
-    this.service.getSearchMovie(this.searchForm.value).subscribe((result)=>{
-      console.log(result,'searchmovie#');
-      this.searchResult=result.results;
+  performSearch(searchValue: string) {
+    this.service.getSearchMovie({movieName: searchValue}).subscribe((result) => {
+      console.log(result, 'searchmovie#');
+      this.searchResult = result.results;
     });
   }
 
+  submitForm(){
+    console.log(this.searchForm.value,'searchform#');
+  }
+
 }
+
